8b5dad7db80f9f7939ca9111303874103f57de7d
houmreu <63812471+houmreu@users.noreply.github.com>
2022-06-10 23:05:06 +0200

    Delete generate_nft.py

--- generate_nft.py
+++
@@ -1,8 +0,0 @@
-from nft_generator import AvatarGenerator
-
-def generate_avatar():
-    generator = AvatarGenerator("./images")
-    generator.generate_avatar(301)
-
-if __name__ == "__main__":
-    generate_avatar()
